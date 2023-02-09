6e06d0e4969db36854bfb68ce0487a61ee7b37e8
infoomics <sbx_94@hotmail.com>
2023-02-09 11:51:22 +0000

    complete task 2

--- db/02-create-books-table.sql
+++ db/02-create-books-table.sql
@@ -1,3 +1,12 @@
 \c my_bookshop;
 
 -- create the books table here
+
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    price_in_pence INT NOT NULL,
+    quantity_in_stock INT NOT NULL,
+    release_date DATE NOT NULL,
+    is_fiction BOOLEAN NOT NULL
+);
