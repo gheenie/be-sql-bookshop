1262a634b9c9842d29cc97a66ea0d1dbe665ac2e
infoomics <sbx_94@hotmail.com>
2023-02-10 12:33:40 +0000

    complete task 11

--- db/11-select-books-with-author.sql
+++ db/11-select-books-with-author.sql
@@ -2,7 +2,12 @@
 
 \echo '\n Here is a list of book titles with the corresponding author:\n'
 
-
+SELECT title, author
+FROM books;
 
 \echo '\n Here is a list of authors without an associated book:\n'
 
+SELECT authors.author_name
+FROM authors LEFT OUTER JOIN books
+ON authors.author_name = books.author
+WHERE books.author IS NULL;
