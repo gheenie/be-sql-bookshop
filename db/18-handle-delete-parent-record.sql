8d11bc9a09b343b14c0d723d7fce24bed529aca2
infoomics <sbx_94@hotmail.com>
2023-02-10 17:27:04 +0000

    complete task 18

--- db/18-handle-delete-parent-record.sql
+++ db/18-handle-delete-parent-record.sql
@@ -1,10 +1,19 @@
 \c my_bookshop
 
+SELECT * FROM authors;
+SELECT * FROM books;
+SELECT * FROM books_genres;
+
 -- Deleting a book:
 -- Modify the books_genres table in section 14 first
 
 -- Delete a book here:
+DELETE FROM books
+WHERE title = 'Nineteen Eighty-Four: A Novel'
+RETURNING *;
 
+SELECT * FROM books;
+SELECT * FROM books_genres;
 
 -- Deleting an author:
 -- Modify the books table in section 02 first
@@ -12,3 +21,10 @@
 -- Modify the books_genres table in section 14 first
 
 -- Delete an author here:
+DELETE FROM authors
+WHERE full_name = 'Jane Austen'
+RETURNING *;
+
+SELECT * FROM authors;
+SELECT * FROM books;
+SELECT * FROM books_genres;
