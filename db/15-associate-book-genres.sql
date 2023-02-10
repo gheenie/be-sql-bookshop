c2b85379f2e70f5e9cb0c26f22cae459385af9c8
infoomics <sbx_94@hotmail.com>
2023-02-10 16:12:03 +0000

    complete task 15

--- db/15-associate-book-genres.sql
+++ db/15-associate-book-genres.sql
@@ -1,2 +1,14 @@
 \c my_bookshop
 
+INSERT INTO books_genres 
+    (book_id, genre_id)
+VALUES 
+    (1, 7),
+    (1, 8),
+    (2, 2),
+    (2, 3),
+    (2, 8),
+    (4, 3),
+    (5, 1),
+    (5, 5),
+    (5, 8);
