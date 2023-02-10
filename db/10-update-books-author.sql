7dc51639f8e0a87534b3b6019f8686f032a92cd5
infoomics <sbx_94@hotmail.com>
2023-02-10 11:54:57 +0000

    complete task 10

--- db/10-update-books-author.sql
+++ db/10-update-books-author.sql
@@ -1,2 +1,16 @@
 \c my_bookshop
 
+UPDATE books
+SET author = inserting.author
+FROM (VALUES
+    ('The Hitchhiker''s Guide to the Galaxy', 'Douglas Adams'),
+    ('The Little Prince', 'Antoine de Saint-Exupéry'),
+    ('The Tale of Peter Rabbit', 'Beatrix Potter'),
+    ('Emma', 'Jane Austen'),
+    ('Nineteen Eighty-Four: A Novel', 'George Orwell'),
+    ('The Handmaid''s Tale', 'Margaret Atwood'),
+    ('The War of the Worlds', 'H. G. Wells'),
+    ('Pride and Prejudice', 'Jane Austen')
+) AS inserting(title, author)
+WHERE inserting.title = books.title
+RETURNING *;
