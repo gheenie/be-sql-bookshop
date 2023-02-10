e4ef9c798739902936df401312f1e4b3abe38fe3
infoomics <sbx_94@hotmail.com>
2023-02-10 16:54:43 +0000

    remove author_id from select

--- db/11-select-books-with-author.sql
+++ db/11-select-books-with-author.sql
@@ -8,7 +8,7 @@ ON books.author_id = authors.author_id;
 
 \echo '\n Here is a list of authors without an associated book:\n'
 
-SELECT title, authors.author_id, full_name
+SELECT title, full_name
 FROM books RIGHT OUTER JOIN authors
 ON books.author_id = authors.author_id
 WHERE title IS NULL;
