3b9bb44518577b2a4a94c660da9825c8b03d37a2
infoomics <sbx_94@hotmail.com>
2023-02-10 12:42:15 +0000

    change primary key; rename column

--- db/07-create-authors-table.sql
+++ db/07-create-authors-table.sql
@@ -1,6 +1,7 @@
 \c my_bookshop
 
 CREATE TABLE authors (
-    author_name varchar(50) PRIMARY KEY,
+    author_id SERIAL PRIMARY KEY,
+    full_name varchar(50) NOT NULL,
     fun_fact TEXT NOT NULL
 );
