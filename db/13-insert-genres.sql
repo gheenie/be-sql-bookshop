1578f7b5fae05d6660972a8bc56201ac6ec48cca
infoomics <sbx_94@hotmail.com>
2023-02-10 13:26:16 +0000

    complete task 13

--- db/13-insert-genres.sql
+++ db/13-insert-genres.sql
@@ -1,3 +1,13 @@
 \c my_bookshop
 
-
+INSERT INTO genres 
+    (genre)
+VALUES 
+    ('science fiction'),
+    ('children''s'),
+    ('romance'),
+    ('fantasy'),
+    ('dystopian'),
+    ('science'),
+    ('adventure'),
+    ('classics');
