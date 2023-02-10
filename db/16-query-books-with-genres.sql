d2f041b0f721b565dbdcd8c1c0fac237e5aebbe0
infoomics <sbx_94@hotmail.com>
2023-02-10 16:12:11 +0000

    complete task 16

--- db/16-query-books-with-genres.sql
+++ db/16-query-books-with-genres.sql
@@ -1,8 +1,17 @@
 \c my_bookshop
 
-\echo '\n Here are the genres for Emma:\n'
+\echo '\n Here are the genres for The Little Prince:\n'
 
+SELECT books_genres.book_genre_id, books.title, genres.genre
+FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE books.title = 'The Little Prince';
 
+\echo '\n Here are all the romance books:\n'
 
-\echo '\n Here are all the dystopian books:\n'
-
+SELECT books_genres.book_genre_id, books.title, genres.genre
+FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE genres.genre = 'romance';
