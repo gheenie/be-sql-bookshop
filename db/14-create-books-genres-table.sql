a843ec380778f57d9bf41465b54401396b87fe0a
infoomics <sbx_94@hotmail.com>
2023-02-10 16:11:56 +0000

    complete task 14

--- db/14-create-books-genres-table.sql
+++ db/14-create-books-genres-table.sql
@@ -1,2 +1,7 @@
 \c my_bookshop
 
+CREATE TABLE books_genres (
+    book_genre_id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(book_id),
+    genre_id INTEGER REFERENCES genres(genre_id)
+);
